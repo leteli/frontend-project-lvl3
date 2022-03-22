c598931b4c6d2d970f056a31408fa6782fd2d25f
Nastia Kartseva <danetzachem@gmail.com>
2022-03-22 21:52:23 +0300

    edit webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,12 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
   mode: process.env.NODE_ENV || 'development',
   module: {
     rules: [
