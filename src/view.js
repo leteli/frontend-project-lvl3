adb95280fc6b7acfbc040e99b30e56dddadca8a4
Nastia Kartseva <danetzachem@gmail.com>
2022-04-04 19:15:59 +0300

    add feeds and posts rendering

--- src/view.js
+++ src/view.js
@@ -5,6 +5,8 @@ import './style.css';
 const watchedState = onChange(state, (path, value) => {
   const form = document.querySelector('.rss-form');
   const input = document.querySelector('#url-input');
+  const feedsEl = document.querySelector('.feeds');
+  const postsEl = document.querySelector('.posts');
   if (path === 'form.valid') {
     if (!value) {
       input.classList.add('is-invalid');
@@ -15,6 +17,37 @@ const watchedState = onChange(state, (path, value) => {
   if (path === 'feeds') {
     form.reset();
     input.focus();
+    feedsEl.innerHTML = '';
+    const feedsHeader = document.createElement('h2');
+    feedsHeader.textContent = 'Фиды'; // положить текст в i18next!
+    feedsEl.prepend(feedsHeader);
+    const feedsList = document.createElement('ul');
+    value.forEach((feed) => {
+      const feedLiEl = document.createElement('li');
+      const feedTitle = document.createElement('h3');
+      feedTitle.textContent = feed.title;
+      const feedDescription = document.createElement('p');
+      feedDescription.textContent = feed.description;
+      feedLiEl.append(feedTitle, feedDescription);
+      feedsList.append(feedLiEl);
+    });
+    feedsEl.append(feedsList);
+  }
+  if (path === 'posts') {
+    postsEl.innerHTML = '';
+    const postsHeader = document.createElement('h2');
+    postsHeader.textContent = 'Посты'; // положить текст в i18next!
+    postsEl.prepend(postsHeader);
+    const postsList = document.createElement('ul');
+    value.forEach((post) => {
+      const postLiEl = document.createElement('li');
+      const postLink = document.createElement('a');
+      postLink.setAttribute('href', post.link);
+      postLink.textContent = post.title;
+      postLiEl.append(postLink);
+      postsList.append(postLiEl);
+    });
+    postsEl.append(postsList);
   }
 });
 
