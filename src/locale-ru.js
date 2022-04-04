df9cfebc3dfecae8151f3b0cb54ca8245fab2c51
Nastia Kartseva <danetzachem@gmail.com>
2022-04-04 19:19:28 +0300

    update texts

--- src/locale-ru.js
+++ src/locale-ru.js
@@ -1,5 +1,8 @@
 export default {
   translation: {
     field_invalid: 'Ссылка должна быть валидным URL',
+    rss_exists: 'RSS-поток уже добавлен',
+    feeds: 'Фиды',
+    posts: 'Посты',
   },
 };
