cbe4c0f45e9abaa13c85d20b34ad1292167f6b88
Nastia Kartseva <danetzachem@gmail.com>
2022-04-16 14:18:22 +0300

    update parser error

--- src/parser.js
+++ src/parser.js
@@ -1,22 +1,20 @@
 export default (data) => {
   const parser = new DOMParser();
-  console.log(typeof data);
   const parsedRss = parser.parseFromString(data, 'application/xml');
   const errorNode = parsedRss.querySelector('parsererror');
   if (errorNode) {
-    throw new Error('invalidRss');
+    const message = errorNode.querySelector('div').textContent;
+    const e = new Error(message);
+    e.name = 'ParserError';
+    throw e;
   }
-  console.log(parsedRss);
   const feedTitle = parsedRss.querySelector('title');
   const feedDescription = parsedRss.querySelector('description');
-  console.log(feedTitle.textContent);
-  console.log(feedDescription.textContent);
   const feed = {
     title: feedTitle.textContent,
     description: feedDescription.textContent,
   };
   const posts = [...parsedRss.querySelectorAll('item')];
-  console.log(posts);
   const postsArr = posts.map((post) => {
     const title = post.querySelector('title');
     const link = post.querySelector('link');
